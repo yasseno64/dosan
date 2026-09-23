ed9018ad27f1bd7a38e7fc635b43f52fa3948329
Yassen Mahmoud <yassenm20178@gmail.com>
2026-09-23 17:37:22 +0300

    V-0

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import HomePage from './pages/HomePage'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import HeroSection from "../components/HeroSection"
+import Navbar from "../components/Navbar"
+
+
+
+
+const HomePage = () => {
+    return (
+        <div>
+        <Navbar/>
+        <main>
+            <HeroSection/>
+        </main>
+        </div>
+    );
+};
+
+export default HomePage;
